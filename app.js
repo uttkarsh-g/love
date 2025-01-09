0f87f82b2bf7e270c1363e46502bb8aca4946f76
Alexhawak <hloji999@gmail.com>
2025-01-09 22:49:14 +0530

    mobile responsive

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const frame = document.body;
+
 frame.addEventListener('mousemove', (position) => {
-  const x = position.offsetX;
-  const y = position.offsetY;
-  const hearts = document.createElement('span');
-  const h = Math.random() * 100;
-  hearts.style.width = h + 'px';
-  hearts.style.height = h + 'px';
-  hearts.style.left = x + 'px';
-  hearts.style.top = y + 'px';
-  frame.appendChild(hearts);
-  setInterval(() => {
-    frame.removeChild(hearts);
-  }, 1000);
+  move(position);
 });
+
+frame.addEventListener('touchmove', (position) => {
+  screenMove(position);
+});
+
+function move(position) {
+  const x = position.offsetX;
+
+  const y = position.offsetY;
+
+  heart(x, y);
+}
+function screenMove(position) {
+  const touch = position.touches[0];
+
+  heart(touch.clientX, touch.clientY);
+}
+function heart(x, y) {
+  const hearts = document.createElement('span');
+
+  const h = Math.random() * 100;
+
+  hearts.style.width = h + 'px';
+
+  hearts.style.height = h + 'px';
+
+  hearts.style.left = x + 'px';
+
+  hearts.style.top = y + 'px';
+
+  frame.appendChild(hearts);
+
+  setInterval(() => {
+    hearts.remove();
+  }, 1000);
+}
